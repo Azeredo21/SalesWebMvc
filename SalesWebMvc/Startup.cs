a202b26f4f7ba69e4a278dff32327cf79ca5a1b2
Gabriel Azeredo <gabriel21azeredo@gmail.com>
2022-07-18 14:21:44 -0300

    add dbContext

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -23,8 +23,9 @@ namespace SalesWebMvc {
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
 
-    services.AddDbContext<SalesWebMvcContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
