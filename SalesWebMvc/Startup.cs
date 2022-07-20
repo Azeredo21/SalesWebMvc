262ea27831de59689ab862ffa8a6c5ccdae0d4e1
Gabriel Azeredo <gabriel21azeredo@gmail.com>
2022-07-20 10:24:42 -0300

    add Seeding Service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -26,16 +26,18 @@ namespace SalesWebMvc {
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                         builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService) {
             if (env.IsDevelopment()) {
+                seedingService.Seed();
                 app.UseDeveloperExceptionPage();
             } else {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
