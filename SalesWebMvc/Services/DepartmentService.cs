07c4a7ab848eee895feceb015220bbd139bb10f5
Gabriel Azeredo <gabriel21azeredo@gmail.com>
2022-08-02 10:18:44 -0300

    add async task

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services {
     public class DepartmentService {
@@ -10,8 +12,9 @@ namespace SalesWebMvc.Services {
         public DepartmentService(SalesWebMvcContext context) {
             _context = context;
         }
-        public List<Department> FindAll() {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+
+        public async Task<List<Department>> FindAllAsync() {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
